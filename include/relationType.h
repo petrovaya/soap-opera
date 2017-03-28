7ec557851e149dcc406aee47c4e8bb7fc9b9f967
Daniil Dotsev <ddocev@uni-sofia.bg>
2017-03-28 23:17:48 +0300

    Add RelationType

---
+++ include/relationType.h
@@ -0,0 +1,19 @@
+#ifndef RELATIONTYPE_H
+#define RELATIONTYPE_H
+
+enum RelationType {
+    MOTHER,
+    FATHER,
+    SON,
+    DAUGHTER,
+    GRANDMOTHER,
+    GRANDFATHER,
+    UNCLE,
+    AUNT,
+    NEPHEW,
+    NIECE,
+    COUSIN,
+    NON_RELATED
+}
+
+#endif
